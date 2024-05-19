303fcd37243a3f92104d13862a6e6d00e1e01bef
Fab <fabiomiguelchenhuang@gmail.com>
2024-05-19 18:21:27 +0100

    server/feat: enabled CORS

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from .config import Config
 from .extensions import db
 from .routes import create_routes
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    CORS(app)
 
     db.init_app(app)
     create_routes(app)
