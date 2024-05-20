b2878dd77691489a7c5c47c454601a9720c1598b
Fab <fabiomiguelchenhuang@gmail.com>
2024-05-20 01:35:36 +0100

    server/feat: added customer count and employee count to office

--- server/app/models/office.py
+++ server/app/models/office.py
@@ -1,7 +1,13 @@
 from ..extensions import db
+from .employee import Employee
+from .customer import Customer
+
+from sqlalchemy.orm import column_property
+from sqlalchemy import select, func
 
 class Office(db.Model):
     __tablename__ = 'offices'
+    
     officeCode = db.Column(db.String(10), primary_key=True)
     city = db.Column(db.String(50), nullable=False)
     phone = db.Column(db.String(50), nullable=False)
@@ -11,6 +17,21 @@ class Office(db.Model):
     country = db.Column(db.String(50), nullable=False)
     postalCode = db.Column(db.String(15), nullable=False)
     territory = db.Column(db.String(10), nullable=False)
+
+    employeeCount = column_property(
+        select(func.count(Employee.employeeNumber))
+        .where(Employee.officeCode == officeCode)
+        .correlate_except(Employee)
+        .scalar_subquery()
+    )
+
+    customerCount = column_property(
+        select(func.count(Customer.customerNumber))
+        .where(Customer.salesRepEmployeeNumber == Employee.employeeNumber)
+        .where(Employee.officeCode == officeCode)
+        .correlate_except(Customer)
+        .scalar_subquery()
+    )
 
     def to_dict(self):
         return {
@@ -22,5 +43,7 @@ class Office(db.Model):
             "state": self.state,
             "country": self.country,
             "postalCode": self.postalCode,
-            "territory": self.territory
+            "territory": self.territory,
+            'employeeCount': self.employeeCount,
+            'customerCount': self.customerCount,
         }
