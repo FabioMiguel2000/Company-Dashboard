6376f79b69a9d3f11a2ef9d5bab289935187a7db
Fab <fabiomiguelchenhuang@gmail.com>
2024-05-22 00:34:12 +0100

    client/refactor: card component

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -10,38 +10,53 @@ import LowStockProducts from "../LowStockProducts/LowStockProducts";
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
+  const cardData = [
+    {
+      to: "/products",
+      title: "Products",
+      count: 110,
+      icon: <BoxSeamFill />,
+      className: "text-bg-primary",
+    },
+    {
+      to: "/customers",
+      title: "Customers",
+      count: 122,
+      icon: <PeopleFill />,
+      className: "text-bg-success",
+    },
+    {
+      to: "/orders",
+      title: "Orders",
+      count: 326,
+      icon: <CartFill />,
+      className: "text-bg-danger",
+    },
+    {
+      to: "/employees",
+      title: "Employees",
+      count: 23,
+      icon: <PersonVcardFill />,
+      className: "text-bg-secondary",
+    },
+  ];
   return (
     <div>
       <h2>Dashboard</h2>
       <div className="main-card-wrapper">
-        <Link to="/products" className="card text-bg-primary mb-3 card-link">
-          <div className="card-body">
-            <h5 className="card-title">Products</h5>
-            <p>110</p>
-          </div>
-          <BoxSeamFill />
-        </Link>
-        <Link to="/customers" className="card text-bg-success mb-3 card-link">
-          <div className="card-body">
-            <h5 className="card-title">Customers</h5>
-            <p>122</p>
-          </div>
-          <PeopleFill />
-        </Link>
-        <Link to="/orders" className="card text-bg-danger mb-3 card-link">
-          <div className="card-body">
-            <h5 className="card-title">Orders</h5>
-            <p>326</p>
-          </div>
-          <CartFill />
-        </Link>
-        <Link to="/employees" className="card text-bg-secondary mb-3 card-link">
-          <div className="card-body">
-            <h5 className="card-title">Employees</h5>
-            <p>23</p>
-          </div>
-          <PersonVcardFill />
-        </Link>
+        {cardData.map((card, index) => (
+          <Link
+            to={card.to}
+            className={`card ${card.className} mb-3 card-link`}
+            key={index}
+          >
+            <div className="card-body">
+              <h5 className="card-title">{card.title}</h5>
+              <p>{card.count}</p>
+            </div>
+            {card.icon}
+          </Link>
+        ))}
       </div>
       <div className="diagrams-wrapper">
         <div className="diagram-container">
