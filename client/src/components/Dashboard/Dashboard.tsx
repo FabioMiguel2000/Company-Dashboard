972987c1da8f2ecf00231f245b9cdcea463d4d2c
Fab <fabiomiguelchenhuang@gmail.com>
2024-05-22 00:26:07 +0100

    client/feat: added links to dashboard cards

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -7,40 +7,41 @@ import {
 } from "react-bootstrap-icons";
 import TopSellingProductsChart from "../TopSellingProductsChart/TopSellingProductsChart";
 import LowStockProducts from "../LowStockProducts/LowStockProducts";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <div>
       <h2>Dashboard</h2>
       <div className="main-card-wrapper">
-        <div className="card text-bg-primary mb-3">
+        <Link to="/products" className="card text-bg-primary mb-3 card-link">
           <div className="card-body">
             <h5 className="card-title">Products</h5>
-            <p>1232</p>
+            <p>110</p>
           </div>
           <BoxSeamFill />
-        </div>
-        <div className="card text-bg-success mb-3">
+        </Link>
+        <Link to="/customers" className="card text-bg-success mb-3 card-link">
           <div className="card-body">
             <h5 className="card-title">Customers</h5>
-            <p>1232</p>
+            <p>122</p>
           </div>
           <PeopleFill />
-        </div>
-        <div className="card text-bg-danger mb-3">
+        </Link>
+        <Link to="/orders" className="card text-bg-danger mb-3 card-link">
           <div className="card-body">
             <h5 className="card-title">Orders</h5>
-            <p>1232</p>
+            <p>326</p>
           </div>
           <CartFill />
-        </div>
-        <div className="card text-bg-secondary mb-3">
+        </Link>
+        <Link to="/employees" className="card text-bg-secondary mb-3 card-link">
           <div className="card-body">
             <h5 className="card-title">Employees</h5>
-            <p>1232</p>
+            <p>23</p>
           </div>
           <PersonVcardFill />
-        </div>
+        </Link>
       </div>
       <div className="diagrams-wrapper">
         <div className="diagram-container">
